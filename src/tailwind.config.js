390037b819c4207ca69192daf6a73b4a7ebd7d90
ilkerkirnaz2-bit <ilkerkirnaz2@gmail.com>
2025-11-12 01:26:18 +0300

    Create tailwind.config.js

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js (KÖK DİZİNDEKİ İÇERİK)
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // src klasörü içindeki tüm jsx, js, tsx, ts dosyalarını tara
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
