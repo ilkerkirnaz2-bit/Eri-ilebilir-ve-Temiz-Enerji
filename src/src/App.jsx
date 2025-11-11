9d5e02b9b2338142b15aff71a0c6a7c2ad9b27b7
ilkerkirnaz2-bit <ilkerkirnaz2@gmail.com>
2025-11-12 00:42:21 +0300

    Delete src/src directory

--- src/src/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div className="text-center p-12 bg-gray-100 min-h-screen">
-      <h1 className="text-4xl font-bold text-green-700 mb-4">Erişilebilir ve Temiz Enerji Girişimi</h1>
-      <p className="text-xl text-gray-600">Altyapı sorunlarını çözdük. İçeriğiniz yayında!</p>
-    </div>
-  );
-}
