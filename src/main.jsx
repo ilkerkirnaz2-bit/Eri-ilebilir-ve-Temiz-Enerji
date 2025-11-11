0754e1680ca8d1275f4ee415e8c517cb71987ed9
ilkerkirnaz2-bit <ilkerkirnaz2@gmail.com>
2025-11-12 00:40:58 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+// Uygulamanın ana CSS dosyasını da buraya dahil etmemiz gerek
+// Netlify'ın bu satırı bulması için
+// import './index.css'; 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
